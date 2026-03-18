9bdf627b2269bfd74a32e25a08f1051867b906b8
EgorBarabash <klim.barabash47@gmail.com>
2026-03-18 22:14:12 +0200

    changed schema.prisma - added new models

--- frontend/src/components/authForm/ProtectedRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-export default function ProtectedRoute() {
-    const isAuth: boolean = false
-    if(isAuth!) {
-        return <Navigate to = "/login" replace></Navigate>
-    }
-    return <div></div>
-}
